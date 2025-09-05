5ca51ad6fbd6983b3b89a5d9fb43b551a084b2f6
felix-t-morgenstern <44931803+felix-t-morgenstern@users.noreply.github.com>
2025-09-05 16:52:26 -0400

    feat: Test and implement ComponentFactory

---
+++ src/main/java/inaugural/soliloquy/io/graphics/renderables/factories/ComponentFactoryImpl.java
@@ -0,0 +1,45 @@
+package inaugural.soliloquy.io.graphics.renderables.factories;
+
+import inaugural.soliloquy.io.graphics.renderables.ComponentImpl;
+import inaugural.soliloquy.tools.Check;
+import soliloquy.specs.common.valueobjects.FloatBox;
+import soliloquy.specs.io.graphics.renderables.Component;
+import soliloquy.specs.io.graphics.renderables.RenderableWithMouseEvents;
+import soliloquy.specs.io.graphics.renderables.factories.ComponentFactory;
+import soliloquy.specs.io.graphics.renderables.providers.ProviderAtTime;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static inaugural.soliloquy.tools.collections.Collections.listOf;
+import static soliloquy.specs.io.input.keyboard.entities.KeyBindingContext.bindingContext;
+
+public class ComponentFactoryImpl implements ComponentFactory {
+    private final ProviderAtTime<FloatBox> RENDERING_BOUNDARIES_PROVIDER;
+    private final Consumer<RenderableWithMouseEvents> ADD_TO_CAPTURING;
+    private final Consumer<RenderableWithMouseEvents> REMOVE_FROM_CAPTURING;
+
+    public ComponentFactoryImpl(ProviderAtTime<FloatBox> renderingBoundariesProvider,
+                                Consumer<RenderableWithMouseEvents> addToCapturing,
+                                Consumer<RenderableWithMouseEvents> removeFromCapturing) {
+        RENDERING_BOUNDARIES_PROVIDER =
+                Check.ifNull(renderingBoundariesProvider, "renderingBoundariesProvider");
+        ADD_TO_CAPTURING = Check.ifNull(addToCapturing, "addToCapturing");
+        REMOVE_FROM_CAPTURING = Check.ifNull(removeFromCapturing, "removeFromCapturing");
+    }
+
+    @Override
+    public Component make(
+            UUID uuid,
+            int z,
+            Component containingComponent
+    ) throws IllegalArgumentException {
+        return new ComponentImpl(
+                Check.ifNull(uuid, "uuid"),
+                z,
+                bindingContext(listOf(), false),
+                Check.ifNull(containingComponent, "containingComponent"),
+                RENDERING_BOUNDARIES_PROVIDER, ADD_TO_CAPTURING, REMOVE_FROM_CAPTURING
+        );
+    }
+}
---
+++ src/test/java/inaugural/soliloquy/io/test/unit/graphics/renderables/factories/ComponentFactoryImplTests.java
@@ -0,0 +1,84 @@
+package inaugural.soliloquy.io.test.unit.graphics.renderables.factories;
+
+import inaugural.soliloquy.io.graphics.renderables.factories.ComponentFactoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import soliloquy.specs.common.valueobjects.FloatBox;
+import soliloquy.specs.io.graphics.renderables.Component;
+import soliloquy.specs.io.graphics.renderables.Renderable;
+import soliloquy.specs.io.graphics.renderables.RenderableWithMouseEvents;
+import soliloquy.specs.io.graphics.renderables.factories.ComponentFactory;
+import soliloquy.specs.io.graphics.renderables.providers.ProviderAtTime;
+
+import java.util.function.Consumer;
+
+import static inaugural.soliloquy.tools.collections.Collections.listOf;
+import static inaugural.soliloquy.tools.random.Random.randomInt;
+import static inaugural.soliloquy.tools.testing.Assertions.once;
+import static java.util.UUID.randomUUID;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ComponentFactoryImplTests {
+    @Mock private ProviderAtTime<FloatBox> mockRenderingBoundaries;
+    @Mock private Consumer<RenderableWithMouseEvents> mockAddToCapturing;
+    @Mock private Consumer<RenderableWithMouseEvents> mockRemoveFromCapturing;
+    @Mock private Component mockComponent;
+
+    private ComponentFactory factory;
+
+    @BeforeEach
+    public void setUp() {
+        factory = new ComponentFactoryImpl(mockRenderingBoundaries, mockAddToCapturing,
+                mockRemoveFromCapturing);
+    }
+
+    @Test
+    public void testConstructorWithInvalidArgs() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new ComponentFactoryImpl(null, mockAddToCapturing, mockRemoveFromCapturing));
+        assertThrows(IllegalArgumentException.class,
+                () -> new ComponentFactoryImpl(mockRenderingBoundaries, null,
+                        mockRemoveFromCapturing));
+        assertThrows(IllegalArgumentException.class,
+                () -> new ComponentFactoryImpl(mockRenderingBoundaries, mockAddToCapturing, null));
+    }
+
+    @Test
+    public void testMake() {
+        var uuid = randomUUID();
+        var z = randomInt();
+        var tier = randomInt();
+        when(mockComponent.tier()).thenReturn(tier);
+        var mockRenderableWithMouseEvents = mock(RenderableWithMouseEvents.class);
+
+        var output = factory.make(uuid, z, mockComponent);
+        output.add(mockRenderableWithMouseEvents);
+        when(mockRenderableWithMouseEvents.component()).thenReturn(output);
+        output.remove(mockRenderableWithMouseEvents);
+
+        assertNotNull(output);
+        assertNotNull(output.keyBindingContext());
+        assertEquals(listOf(), output.keyBindingContext().BINDINGS);
+        assertFalse(output.keyBindingContext().BLOCKS_LOWER_BINDINGS);
+        assertEquals(uuid, output.uuid());
+        assertEquals(z, output.getZ());
+        assertEquals(tier + 1, output.tier());
+        assertSame(mockComponent, output.component());
+        assertSame(mockRenderingBoundaries, output.getRenderingBoundariesProvider());
+        verify(mockAddToCapturing, once()).accept(mockRenderableWithMouseEvents);
+        verify(mockRemoveFromCapturing, once()).accept(mockRenderableWithMouseEvents);
+    }
+
+    @Test
+    public void testMakeWithInvalidArgs() {
+        assertThrows(IllegalArgumentException.class,
+                () -> factory.make(null, randomInt(), mockComponent));
+        assertThrows(IllegalArgumentException.class,
+                () -> factory.make(randomUUID(), randomInt(), null));
+    }
+}
